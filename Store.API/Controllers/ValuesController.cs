7daf04e4467dc6855dde8ed55c69c3980ebdc644
Rob Schrage <rjschrage@arcurve.com>
2019-01-23 09:36:34 -0700

    Upgraded to .NET 4.6.1

--- Store.API/Controllers/ValuesController.cs
+++ Store.API/Controllers/ValuesController.cs
@@ -12,13 +12,13 @@ namespace Store.API.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2" , "value3"};
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
